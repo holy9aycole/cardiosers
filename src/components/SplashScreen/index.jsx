74097b37844812e6ae4f37ec89474b451e6cf452
Aditi0310 <aditi.tiwari0310@gmail.com>
2021-11-29 18:41:23 +0530

    changes

--- src/components/SplashScreen/index.jsx
+++ src/components/SplashScreen/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Grid, Typography } from "@mui/material";
-import { useStyles } from "./styles";
+import { useStyles, Button, ButtonContainer, ButtonLine } from "./styles";
 import Image1 from '../../assets/images/splash-web.png'
 import Logo from '../../assets/images/rmz-logo (1).svg'
 import Image2 from '../../assets/images/MaskGroup6.svg'
+import buttonArrow from '../../assets/images/button-arrow.svg'
+import buttonLine from '../../assets/images/button-line-2.svg'
 
 function SplashScreen() {
   const classes = useStyles();
@@ -17,6 +19,8 @@ function SplashScreen() {
         <Grid item sm={6} className={classes.container2}>
           <img src={Logo} alt="" className={classes.logo}/>
           <Typography className={classes.text1}>CRAFTING SPACES<br/> THAT CONNECT<br/> SOCIALLY</Typography>
+          <ButtonContainer><Button src={buttonArrow} alt=""/></ButtonContainer>
+          <ButtonLine src={buttonLine} alt=""/>
           <img src={Image2} alt="" className={classes.image2}/>
         </Grid>
       </Grid>
--- src/components/SplashScreen/styles.js
+++ src/components/SplashScreen/styles.js
@@ -1,4 +1,41 @@
 import { makeStyles } from "@mui/styles";
+import {styled} from '@mui/material/styles'
+
+export const Button = styled('img')(({theme}) => ({
+  width:"12px",
+  marginLeft:"18px",
+  marginTop:"12px",
+  [theme.breakpoints.down('sm')]:{
+      margin:"120px auto",
+      width:"100vw"
+  }
+}));
+
+export const ButtonContainer = styled('div')(({theme}) => ({
+  width:"70px",
+  height:"70px",
+  backgroundColor:"#3d3dd9",
+  padding:"10px",
+  borderRadius:"50%",
+  position:"realtive",
+  marginLeft:"10%",
+  marginTop:"50px",
+  boxShadow: '0px 15px 20px #00000033',
+  [theme.breakpoints.down('sm')]:{
+      margin:"120px auto",
+      width:"100vw"
+  }
+}));
+
+export const ButtonLine = styled('img')(({theme}) => ({
+ position:"relative",
+ left:"16%",
+ top:"-90px",
+  [theme.breakpoints.down('sm')]:{
+      margin:"120px auto",
+      width:"100vw"
+  }
+}));
 
 export const useStyles = makeStyles(() => ({
   image1: {
