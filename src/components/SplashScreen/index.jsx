e71a9be2848f9c205df0d2741f8b0cf6624932e3
Aditi0310 <aditi.tiwari0310@gmail.com>
2021-11-29 18:58:03 +0530

    changes

--- src/components/SplashScreen/index.jsx
+++ src/components/SplashScreen/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Grid, Typography } from "@mui/material";
-import { useStyles, Button, ButtonContainer, ButtonLine } from "./styles";
+import { Grid } from "@mui/material";
+import { useStyles, Button, ButtonContainer, ButtonLine, Text1 , Container2} from "./styles";
 import Image1 from '../../assets/images/splash-web.png'
 import Logo from '../../assets/images/rmz-logo (1).svg'
 import Image2 from '../../assets/images/MaskGroup6.svg'
@@ -16,13 +16,13 @@ function SplashScreen() {
         <Grid item sm={6}>
           <img src={Image1} alt="" className={classes.image1}/>
         </Grid>
-        <Grid item sm={6} className={classes.container2}>
+        <Container2 item sm={6}>
           <img src={Logo} alt="" className={classes.logo}/>
-          <Typography className={classes.text1}>CRAFTING SPACES<br/> THAT CONNECT<br/> SOCIALLY</Typography>
+          <Text1 >CRAFTING SPACES<br/> THAT CONNECT<br/> SOCIALLY</Text1>
           <ButtonContainer><Button src={buttonArrow} alt=""/></ButtonContainer>
           <ButtonLine src={buttonLine} alt=""/>
           <img src={Image2} alt="" className={classes.image2}/>
-        </Grid>
+        </Container2>
       </Grid>
     </div>
   );
--- src/components/SplashScreen/styles.js
+++ src/components/SplashScreen/styles.js
@@ -1,13 +1,13 @@
 import { makeStyles } from "@mui/styles";
 import {styled} from '@mui/material/styles'
+import { Typography, Grid } from "@mui/material";
 
 export const Button = styled('img')(({theme}) => ({
   width:"12px",
   marginLeft:"18px",
   marginTop:"12px",
   [theme.breakpoints.down('sm')]:{
-      margin:"120px auto",
-      width:"100vw"
+      
   }
 }));
 
@@ -17,13 +17,12 @@ export const ButtonContainer = styled('div')(({theme}) => ({
   backgroundColor:"#3d3dd9",
   padding:"10px",
   borderRadius:"50%",
-  position:"realtive",
+  position:"relative",
   marginLeft:"10%",
   marginTop:"50px",
   boxShadow: '0px 15px 20px #00000033',
-  [theme.breakpoints.down('sm')]:{
-      margin:"120px auto",
-      width:"100vw"
+  [theme.breakpoints.down('xs')]:{
+      marginLeft:"90%",
   }
 }));
 
@@ -34,6 +33,25 @@ export const ButtonLine = styled('img')(({theme}) => ({
   [theme.breakpoints.down('sm')]:{
       margin:"120px auto",
       width:"100vw"
+  }
+}));
+
+export const Text1 = styled(Typography)(({theme}) => ({
+  fontSize:"50px",
+  color:"#ffffff",
+  marginLeft:"10%",
+  marginTop:"60px",
+  [theme.breakpoints.down('sm')]:{
+      fontSize:"30px",
+  }
+}));
+
+export const Container2 = styled(Grid)(({theme}) => ({
+  backgroundColor: "#2C2C7E",
+  zIndex:10,
+  
+  [theme.breakpoints.down('xs')]:{
+      borderTopLeftRadius:"50px",
   }
 }));
 
