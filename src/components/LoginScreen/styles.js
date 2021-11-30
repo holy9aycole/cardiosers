06a4c74729bf4be5bdc4cb41db749c742569f6df
Aditi0310 <aditi.tiwari0310@gmail.com>
2021-11-30 18:39:31 +0530

    changes

--- src/components/LoginScreen/styles.js
+++ src/components/LoginScreen/styles.js
@@ -1,6 +1,29 @@
 import { makeStyles } from "@mui/styles";
+import {styled} from '@mui/material/styles';
 import LoginBg from '../../assets/images/login-web.png'
 
+export const FormContainer = styled('div')(({theme}) => ({
+  border: '1px solid #3D3DD9',
+  borderRadius: '30px',
+  opacity: 1,
+  backgroundColor:"#0f15a2",
+  mixBlendMode:"luminosity",
+  width:"40vw",
+  padding:"80px 70px 40px 70px",
+  margin:"40px auto",
+  display:"block",
+  [theme.breakpoints.down('sm')]:{
+    width:"100vw",
+    position:"absolute",
+    bottom:0,
+    margin:0,
+    borderBottomLeftRadius:0,
+    borderBottomRightRadius:0,
+    backgroundColor:"#2e2e80",
+    mixBlendMode:"normal",
+    padding:"80px 30px 100px 30px"
+  }
+}));
 
 export const useStyles = makeStyles((theme) => ({
   mainContainer:{
@@ -17,6 +40,9 @@ export const useStyles = makeStyles((theme) => ({
     width:"100px",
     [theme.breakpoints.up('xl')]:{
       width:"160px",
+    },
+    [theme.breakpoints.down('sm')]:{
+      width:"120px",
     }
   },
   formContainer:{
@@ -24,10 +50,13 @@ export const useStyles = makeStyles((theme) => ({
     borderRadius: '30px',
     opacity: 1,
     backgroundColor:"#0f15a2",
-     mixBlendMode:"luminosity",
+    mixBlendMode:"luminosity",
     width:"40vw",
     padding:"80px 70px 40px 70px",
-    margin:"40px auto"
+    margin:"40px auto",
+    [theme.breakpoints.down('xs')]:{
+      position:"absolute"
+    }
   },
   button:{
     backgroundColor:"#3d3dd9",
