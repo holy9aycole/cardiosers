df8c67fc49e60056218fb0b667ff837c876582a1
Aditi0310 <aditi.tiwari0310@gmail.com>
2021-12-01 15:29:34 +0530

    changes

--- src/Screens/Property/styles.js
+++ src/Screens/Property/styles.js
@@ -1,4 +1,3 @@
-import { makeStyles } from "@mui/styles";
 import { styled } from "@mui/material/styles";
 import { Typography } from "@mui/material";
 import Bg from '../../assets/images/Map-bg-web.png'
@@ -42,61 +41,4 @@ export const Text1 = styled(Typography)(({theme}) => ({
 
 
 
- export const useStyles = makeStyles((theme) => ({
-    mainContainer:{
-        backgroundImage:`url(${Bg})`,
-        backgroundRepeat:"no-repeat",
-        height:"100vh",
-        backgroundSize:"cover",
-        paddingTop:"140px",
-        [theme.breakpoints.down('xs')]:{
-            height:"100vw",
-        }
-    },
-    indiaMap:{
-        display:"block",
-        margin:"0px auto",
-        width:"50vw",
-        [theme.breakpoints.down('xs')]:{
-            margin:"50px auto",
-            width:"100vw"
-        }
-    },
-    location1:{
-        position:"fixed",
-        left:"45.5%",
-        marginTop:"-26%",
-        display:"block",
-        [theme.breakpoints.down('sm')]:{
-            display:"none"
-        }
-    },
-    locationPoint1:{
-        position:"absolute",
-        left:"50%",
-        marginTop:"-8%",
-        [theme.breakpoints.down('sm')]:{
-            display:"none"
-        }
-    },
-    border1:{
-        borderLeft:"1px solid blue",
-        position:"fixed",
-        left:"51%",
-        height:"200px",
-        marginTop:"-23%",
-        [theme.breakpoints.down('sm')]:{
-            display:"none"
-        }
-    },
-    text1:{
-        color:"#3d3dd9",
-        fontSize:"48px",
-        fontFamily:"IvyPresto",
-        letterSpacing:"0.48px",
-        lineHeight:"50px",
-        position:"absolute",
-        marginLeft:"60px"
-    }
-}))
-
+ 
