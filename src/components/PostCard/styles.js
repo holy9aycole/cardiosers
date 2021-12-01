e610869aa0ffc2393f4961d58bafde140120e7e9
Shourya Shikhar <shouryashikhar@gmail.com>
2021-12-01 23:03:32 +0530

    Post CArd

--- src/components/PostCard/styles.js
+++ src/components/PostCard/styles.js
@@ -1,5 +1,4 @@
-import { styled } from "@mui/styles";
-import { Card,Button,Typography } from "@mui/material"
+import { Card,Button,Typography,styled } from "@mui/material"
 
 export const StyledCard = styled(Card)(({ theme }) => ({
     width: "40%",
@@ -8,13 +7,13 @@ export const StyledCard = styled(Card)(({ theme }) => ({
     borderRadius: "4px",
     background: "#FFFFFF",
     [theme.breakpoints.down("md")]: {
-        width: "100%",
+        width: "95%"
     },
 }));
 
-export const StyledContentDiv = styled("div")({
-    backgroundImage: "url(../../assets/Mask Group 18.svg)",
-});
+export const StyledContentDiv = styled("div")(()=>({
+    backgroundImage: "url(assets/Mask Group 18.svg)",
+}));
 
 export const StyledButton = styled(Button)({
     position: "absolute",
@@ -68,9 +67,7 @@ export const FooterDiv = styled("div")({
     marginRight: 5,
 });
 
-export const FooterFont = styled(
-    <Typography color="primary" />
-)({
+export const FooterFont = styled(Typography)({
     padding: "0px 16px",
     fontSize: 13,
     color: "#3D3DD9",
