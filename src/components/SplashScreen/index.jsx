fa71313ae4f1ed4a1e46ea316412f6300f9104c5
Ashish Yadav <yadavashishkumar97@gmail.com>
2021-12-07 23:35:06 +0530

    changes

--- src/components/SplashScreen/index.jsx
+++ src/components/SplashScreen/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Grid } from "@mui/material";
-import { Button1, ButtonContainer, ButtonLine, Text1 , Container2, MainContainer, Image1, Logo, Image2, SkipButton} from "./styles";
+import { Button1, ButtonContainer, ButtonLine, Text1, Container2, MainContainer, Image1, Logo, Image2, SkipButton } from "./styles";
 import image1 from '../../assets/images/splash-web.png'
 import logo from '../../assets/images/rmz-logo (1).svg'
 import image2 from '../../assets/images/MaskGroup6.svg'
@@ -9,20 +9,20 @@ import buttonArrow from '../../assets/images/button-arrow.svg'
 import buttonLine from '../../assets/images/button-line-2.svg'
 
 function SplashScreen() {
-  
+
   return (
     <MainContainer>
       <Grid container>
         <Grid item sm={6}>
-          <Image1 src={image1} alt=""/>
+          <Image1 src={image1} alt="" />
         </Grid>
         <Container2 item sm={6}>
-          <Logo src={logo} alt=""/>
-          <Text1 >CRAFTING SPACES<br/> THAT CONNECT<br/> SOCIALLY</Text1>
-          <SkipButton>SKIP</SkipButton>
-          <ButtonContainer><Button1 src={buttonArrow} alt=""/></ButtonContainer>
-          <ButtonLine src={buttonLine} alt=""/>
-          <Image2 src={image2} alt=""/>
+          <Logo src={logo} alt="" />
+          <Text1 >CRAFTING SPACES<br /> THAT CONNECT<br /> SOCIALLY</Text1>
+          <SkipButton disableRipple>SKIP</SkipButton>
+          <ButtonContainer><Button1 src={buttonArrow} alt="" /></ButtonContainer>
+          <ButtonLine src={buttonLine} alt="" />
+          <Image2 src={image2} alt="" />
         </Container2>
       </Grid>
     </MainContainer>
--- src/components/SplashScreen/styles.js
+++ src/components/SplashScreen/styles.js
@@ -1,110 +1,116 @@
-import {styled} from '@mui/material/styles'
+import { styled } from '@mui/material/styles'
 import { Typography, Grid, Button } from "@mui/material";
 
 export const MainContainer = styled('div')(() => ({
-  height:"100vh",
-  overflow:"hidden",
-  position:"fixed",
-  width:"100vw"
+  height: "100vh",
+  overflow: "hidden",
+  position: "fixed",
+  width: "100vw"
 }));
 
-export const Button1 = styled('img')(({theme}) => ({
-  width:"12px",
-  marginLeft:"18px",
-  marginTop:"12px",
-  [theme.breakpoints.down('sm')]:{
-      width:"10px",
-      marginLeft:"15px"
+export const Button1 = styled('img')(({ theme }) => ({
+  width: "12px",
+  marginLeft: "18px",
+  marginTop: "12px",
+  [theme.breakpoints.down('sm')]: {
+    width: "10px",
+    marginLeft: "15px"
   }
 }));
 
-export const ButtonContainer = styled('div')(({theme}) => ({
-  width:"70px",
-  height:"70px",
-  backgroundColor:"#3d3dd9",
-  padding:"10px",
-  borderRadius:"50%",
-  position:"relative",
-  marginLeft:"10%",
-  marginTop:"50px",
+export const ButtonContainer = styled('div')(({ theme }) => ({
+  width: "70px",
+  height: "70px",
+  backgroundColor: "#3d3dd9",
+  padding: "10px",
+  borderRadius: "50%",
+  position: "relative",
+  marginLeft: "10%",
+  marginTop: "100px",
   boxShadow: '0px 15px 20px #00000033',
-  [theme.breakpoints.down('sm')]:{
-      marginLeft:"70%",
-      width:"60px",
-      height:"60px",
-      marginTop:"10px"
+  [theme.breakpoints.down('sm')]: {
+    marginLeft: "70%",
+    width: "60px",
+    height: "60px",
+    marginTop: "10px"
   }
 }));
 
-export const ButtonLine = styled('img')(({theme}) => ({
- position:"relative",
- left:"16%",
- top:"-90px",
- display:"block",
-  [theme.breakpoints.down('sm')]:{
-      margin:"120px auto",
-      width:"100vw",
-      display:"none"
+export const ButtonLine = styled('img')(({ theme }) => ({
+  position: "relative",
+  left: "16%",
+  top: "-90px",
+  display: "block",
+  [theme.breakpoints.down('sm')]: {
+    margin: "120px auto",
+    width: "100vw",
+    display: "none"
   }
 }));
 
-export const Text1 = styled(Typography)(({theme}) => ({
-  fontSize:"50px",
-  color:"#ffffff",
-  marginLeft:"10%",
-  marginTop:"60px",
-  fontFamily:"IvyPresto",
-  [theme.breakpoints.down('sm')]:{
-      fontSize:"25px",
-      marginTop:"30px"
+export const Text1 = styled(Typography)(({ theme }) => ({
+  fontSize: "50px",
+  color: "#ffffff",
+  marginLeft: "10%",
+  marginTop: "100px",
+  fontFamily: "IvyPresto",
+  [theme.breakpoints.down('sm')]: {
+    fontSize: "32px",
+    lineHeight: '40px',
+    marginTop: "50px",
+    letteSpacing: '0.32px'
   }
 }));
 
-export const Container2 = styled(Grid)(({theme}) => ({
+export const Container2 = styled(Grid)(({ theme }) => ({
   backgroundColor: "#2C2C7E",
-  zIndex:10,
-  [theme.breakpoints.down('sm')]:{
-      borderTopLeftRadius:"50px",
-      borderTopRightRadius:"50px",
-      marginTop:"-70px",
-      width:"100vw",
-      height:"600px",
+  zIndex: 10,
+  [theme.breakpoints.down('sm')]: {
+    borderTopLeftRadius: "50px",
+    borderTopRightRadius: "50px",
+    marginTop: "-70px",
+    width: "100vw",
+    height: "600px",
+    paddingTop: '20px'
   }
 }));
 
-export const Image1 = styled('img')(({theme}) => ({
+export const Image1 = styled('img')(({ theme }) => ({
   width: "100%",
-  height:"auto",
-  [theme.breakpoints.down('sm')]:{
-    height:"60vh",
-    width:"100vw"
+  height: "100vh",
+  [theme.breakpoints.down('sm')]: {
+    height: "60vh",
+    width: "100vw"
   }
 }));
 
-export const Logo = styled('img')(({theme}) => ({
+export const Logo = styled('img')(({ theme }) => ({
   width: "15%",
-  marginTop:"12%",
-  marginLeft:"10%",
-  [theme.breakpoints.down('sm')]:{
-    
+  marginTop: "12%",
+  marginLeft: "10%",
+  [theme.breakpoints.down('sm')]: {
+    display: 'none'
   }
 }));
 
 export const Image2 = styled('img')(() => ({
-    position:"absolute",
-    bottom:0,
-    zIndex:-1
+  position: "absolute",
+  bottom: 0,
+  zIndex: -1
 }));
 
-export const SkipButton = styled(Button)(({theme}) => ({
-  position:"relative",
-    top:"50px",
-    left:"10%",
-    color:"white",
-    display:"none",
-    [theme.breakpoints.down('sm')]:{
-      display:"block"
-    }
+export const SkipButton = styled(Button)(({ theme }) => ({
+  position: "relative",
+  top: "50px",
+  left: "10%",
+  color: "white",
+  display: "none",
+  [theme.breakpoints.down('sm')]: {
+    display: "block",
+    fontSize: "14px",
+    lineHeight: '20px',
+    letteSpacing: '0.48px'
+  }
 }));
 
 
