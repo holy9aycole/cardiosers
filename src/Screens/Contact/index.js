047e25b287dfd3d0764ef6edd620f2dfbf3d240d
Shourya Shikhar <shouryashikhar@gmail.com>
2021-12-23 01:43:27 +0530

    Contact Page

--- src/Screens/Contact/index.js
+++ src/Screens/Contact/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import {} from "./styles";
+import { ReactComponent as PhoneIcon } from "assets/icons/Contact/phone-icon.svg";
+import { ReactComponent as EmailIcon } from "assets/icons/Contact/email-icon.svg";
+import { Container, ContactCard, CustomButton } from "./styles";
 
 function Contact() {
+	const handlePhone = () => {
+		window.open("tel:08040004000");
+	};
+	const handleEmail = () => {
+		window.open("mailto:leasingblr@rmzcorp.com");
+	};
+
 	return (
-		<div style={{ height: "100vh", width: "100%" }}>
+		<Container>
 			<GoogleMapReact
 				defaultCenter={{
 					lat: 12.9716,
@@ -12,7 +21,15 @@ function Contact() {
 				}}
 				defaultZoom={10}
 			/>
-		</div>
+			<ContactCard>
+				<CustomButton>
+					<PhoneIcon onClick={handlePhone} />
+				</CustomButton>
+				<CustomButton>
+					<EmailIcon onClick={handleEmail} />
+				</CustomButton>
+			</ContactCard>
+		</Container>
 	);
 }
 
