bf5e1acee6aea97d2575d55427efffbefeaf2933
Shourya Shikhar <shouryashikhar@gmail.com>
2021-12-23 01:01:48 +0530

    Removed Marker

--- src/Screens/Contact/index.js
+++ src/Screens/Contact/index.js
@@ -1,20 +1,8 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import { ReactComponent as RMZLogo } from "assets/icons/Contact/rmz-logo.svg";
-import { ReactComponent as MapPin } from "assets/icons/Contact/map-pin.svg";
-import { } from "./styles";
+import {} from "./styles";
 
 function Contact() {
-
-
-	const CustomMarker = ({ text }) => (
-		<div>
-			<MapPin />
-			<RMZLogo />
-			{text}
-		</div>
-	);
-
 	return (
 		<div style={{ height: "100vh", width: "100%" }}>
 			<GoogleMapReact
@@ -23,9 +11,7 @@ function Contact() {
 					lng: 77.5946,
 				}}
 				defaultZoom={10}
-			>
-				<CustomMarker lat={12.9716} lng={77.5946} text="Test" />
-			</GoogleMapReact>
+			/>
 		</div>
 	);
 }
