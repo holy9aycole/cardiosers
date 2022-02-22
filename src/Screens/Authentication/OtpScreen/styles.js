432ba533d67bc6c016c25cc022213f416ff15e5f
Ashish Yadav <yadavashishkumar97@gmail.com>
2022-02-22 16:23:44 +0530

    property page integration

--- src/Screens/Authentication/OtpScreen/styles.js
+++ src/Screens/Authentication/OtpScreen/styles.js
@@ -2,148 +2,147 @@ import { styled } from "@mui/material/styles";
 import { Button, TextField, Typography } from "@mui/material";
 import LoginBg from "assets/images/login-web.png";
 
-
-export const FormContainer = styled('div')(({ theme }) => ({
-	border: '1px solid #3D3DD9',
-	borderRadius: '30px',
-	opacity: 1,
-	backgroundColor: "#0f15a2",
-	mixBlendMode: "luminosity",
-	width: "40vw",
-	padding: "50px 70px 20px 70px",
-	margin: "40px auto",
-	display: "block",
-	[theme.breakpoints.down('sm')]: {
-		width: "100vw",
-		position: "absolute",
-		bottom: 0,
-		margin: 0,
-		borderBottomLeftRadius: 0,
-		borderBottomRightRadius: 0,
-		backgroundColor: "#2e2e80",
-		mixBlendMode: "normal",
-		padding: "80px 30px 50px 30px"
-	},
-	'& .MuiOutlinedInput-root': {
-		'& fieldset': {
-			borderColor: 'grey',
-		},
-		'&:hover fieldset': {
-			borderColor: 'grey',
-		},
-	},
-	'& input': {
-		color: 'white',
-	}
+export const FormContainer = styled("div")(({ theme }) => ({
+  border: "1px solid #3D3DD9",
+  borderRadius: "30px",
+  opacity: 1,
+  backgroundColor: "#0f15a2",
+  mixBlendMode: "luminosity",
+  width: "40vw",
+  padding: "50px 70px 20px 70px",
+  margin: "40px auto",
+  display: "block",
+  [theme.breakpoints.down("sm")]: {
+    width: "100vw",
+    position: "absolute",
+    bottom: 0,
+    margin: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    backgroundColor: "#2e2e80",
+    mixBlendMode: "normal",
+    padding: "80px 30px 50px 30px",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "grey",
+    },
+    "&:hover fieldset": {
+      borderColor: "grey",
+    },
+  },
+  "& input": {
+    color: "white",
+  },
 }));
 
-export const MainContainer = styled('div')(() => ({
-	backgroundImage: `url(${LoginBg})`,
-	backgroundRepeat: "no-repeat",
-	backgroundSize: "cover",
-	height: "100vh",
-	paddingTop: "20px",
+export const MainContainer = styled("div")(() => ({
+  backgroundImage: `url(${LoginBg})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100vh",
+  paddingTop: "20px",
 }));
 
 export const Button1 = styled(Button)(() => ({
-	backgroundColor: "#3d3dd9",
-	color: "#ffffff",
-	width: "100%",
-	opacity: 1,
-	marginTop: "2rem",
-	position: "relative",
-	height: "45px",
-	textDecorationLine: "none",
-	"&:hover": {
-		backgroundColor: "#3d3dd9",
-		filter: "brightness(120%)",
-	},
+  backgroundColor: "#3d3dd9",
+  color: "#ffffff",
+  width: "100%",
+  opacity: 1,
+  marginTop: "2rem",
+  position: "relative",
+  height: "45px",
+  textDecorationLine: "none",
+  "&:hover": {
+    backgroundColor: "#3d3dd9",
+    filter: "brightness(120%)",
+  },
 }));
 
 export const TextField1 = styled(TextField)(() => ({
-	width: "100%",
-	borderRadius: "6px",
-	backgroundColor: "#2E2E80",
-	filter: "contrast(120%)",
-	"& .MuiOutlinedInput-root": {
-		borderRadius: 8,
-		color: "#FFF",
-		borderColor: "#FFF",
-	},
+  width: "100%",
+  borderRadius: "6px",
+  backgroundColor: "#2E2E80",
+  filter: "contrast(120%)",
+  "& .MuiOutlinedInput-root": {
+    borderRadius: 8,
+    color: "#FFF",
+    borderColor: "#FFF",
+  },
 }));
 
 export const Text1 = styled(Typography)(() => ({
-	color: "#ffffff",
-	fontSize: "14px",
-	textAlign: "center",
-	marginTop: "10px",
-	opacity: 0.8,
+  color: "#ffffff",
+  fontSize: "14px",
+  textAlign: "center",
+  marginTop: "10px",
+  opacity: 0.8,
 }));
 
 export const MailIcon = styled("img")(() => ({
-	position: "relative",
-	left: "92%",
-	width: 25,
-	height: 20,
-	marginBottom: "-40px",
-	zIndex: 1,
+  position: "relative",
+  left: "92%",
+  width: 25,
+  height: 20,
+  marginBottom: "-40px",
+  zIndex: 1,
 }));
 
 export const ResendButton = styled(Button)(() => ({
-	color: "#ffffff",
-	display: "block",
-	marginLeft: "auto",
-	marginRight: "auto",
-	marginTop: "20px",
+  color: "#ffffff",
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: "20px",
 }));
 
 export const OtpInnerContainer = styled("div")(() => ({
-	width: "70px",
-	height: "56px",
-	backgroundColor: "#3d3dd9",
-	borderRadius: "10px",
-	textAlign: "center",
-	color: "white",
-	fontSize: "24px",
+  width: "70px",
+  height: "56px",
+  backgroundColor: "#3d3dd9",
+  borderRadius: "10px",
+  textAlign: "center",
+  color: "white",
+  fontSize: "24px",
 }));
 
 export const OtpContainer = styled("div")(() => ({
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "space-between",
-	marginTop: "25px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  marginTop: "25px",
 }));
 
 export const OtpTextField = styled(TextField)(() => ({
-	fontSize: "30px",
-	borderRadius: "10px",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-	"& .MuiOutlinedInput-root": {
-		"& input": {
-			color: "#fff",
-			textAlign: "center",
-		},
-		"& fieldset": {
-			borderColor: "grey",
-			borderRadius: "10px",
-		},
-		"&:hover fieldset": {
-			borderColor: "grey",
-			borderRadius: "10px",
-		},
-	},
+  fontSize: "30px",
+  borderRadius: "10px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  "& .MuiOutlinedInput-root": {
+    "& input": {
+      color: "#fff",
+      textAlign: "center",
+    },
+    "& fieldset": {
+      borderColor: "grey",
+      borderRadius: "10px",
+    },
+    "&:hover fieldset": {
+      borderColor: "grey",
+      borderRadius: "10px",
+    },
+  },
 }));
 
 export const Logo = styled("img")(({ theme }) => ({
-	display: "block",
-	margin: "auto",
-	width: "100px",
-	[theme.breakpoints.up("xl")]: {
-		width: "160px",
-	},
-	[theme.breakpoints.down("sm")]: {
-		width: "120px",
-	},
+  display: "block",
+  margin: "auto",
+  width: "100px",
+  [theme.breakpoints.up("xl")]: {
+    width: "160px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    width: "120px",
+  },
 }));
--- src/Screens/PropertyCity/styles.js
+++ src/Screens/PropertyCity/styles.js
@@ -1,305 +1,329 @@
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import FileDownloadIcon from "@mui/icons-material/FileDownload";
 
 const {
-	styled,
-	Select,
-	Accordion,
-	AccordionSummary,
-	Typography,
-	CardContent,
-	AccordionDetails,
-	Card,
+  styled,
+  Select,
+  Accordion,
+  AccordionSummary,
+  Typography,
+  CardContent,
+  AccordionDetails,
+  Card,
 } = require("@mui/material");
 
 export const PropertyPage = styled("div")({
-	height: "100%",
-	marginBottom: "100vh",
+  height: "100%",
+  marginBottom: "100vh",
 });
 
 export const BannerContainer = styled("div")(({ theme }) => ({
-	width: "100%",
-	height: "233px",
-	overflow: "hidden",
-	marginTop: 81,
-	display: "flex",
-	flexDirection: "column",
-	alignItems: "center",
-	justifyContent: "center",
-	marginBottom: 20,
-	[theme.breakpoints.down("md")]: {
-		marginTop: 60,
-	},
-	[theme.breakpoints.down("sm")]: {
-		height: 100,
-	},
+  width: "100%",
+  height: "233px",
+  overflow: "hidden",
+  marginTop: 81,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  marginBottom: 20,
+  [theme.breakpoints.down("md")]: {
+    marginTop: 60,
+  },
+  [theme.breakpoints.down("sm")]: {
+    height: 100,
+  },
 }));
 
 export const BannerImage = styled("img")(({ theme }) => ({
-	position: "absolute",
-	width: "100%",
-	height: 233,
-	[theme.breakpoints.down("sm")]: {
-		display: "none",
-	},
+  position: "absolute",
+  width: "100%",
+  height: 233,
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
 }));
 
 export const Overlay = styled("div")(({ theme }) => ({
-	width: "100%",
-	background: "rgba(61,61,217,0.7)",
-	height: 233,
-	position: "absolute",
-	top: 81,
-	[theme.breakpoints.down("md")]: {
-		top: 60,
-	},
-	[theme.breakpoints.down("sm")]: {
-		display: "none",
-	},
-	zIndex: 2,
+  width: "100%",
+  background: "rgba(61,61,217,0.7)",
+  height: 233,
+  position: "absolute",
+  top: 81,
+  [theme.breakpoints.down("md")]: {
+    top: 60,
+  },
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+  zIndex: 2,
 }));
 
 export const StyledSelect = styled(Select)(({ theme }) => ({
-	width: "30%",
-	marginTop: 20,
-	zIndex: 3,
-	color: "#fff",
-	background: "rgba(0,0,0,0.2)",
-	[theme.breakpoints.down("sm")]: {
-		width: "75%",
-		background: "#FFF",
-		color: "#000",
-	},
+  width: "30%",
+  marginTop: 20,
+  zIndex: 3,
+  color: "#fff",
+  background: "rgba(0,0,0,0.2)",
+  [theme.breakpoints.down("sm")]: {
+    width: "75%",
+    background: "#FFF",
+    color: "#000",
+  },
 }));
 
 export const StyledSelectIcon = styled(ArrowDropDownIcon)({
-	color: "#3D3DD9",
+  color: "#3D3DD9",
 });
 
 export const AccordionContainer = styled("div")({
-	display: "flex",
-	flexDirection: "column",
-	alignItems: "center",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
 });
 
 export const StyledAccordion = styled(Accordion)(({ theme }) => ({
-	width: "90%",
-	padding: 20,
-	boxShadow: "none!important",
-	background: "transparent",
-	[theme.breakpoints.down("sm")]: {
-		width: "100%",
-	}
+  width: "90%",
+  padding: 20,
+  boxShadow: "none!important",
+  background: "transparent",
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+    padding: 0,
+  },
 }));
 
 export const StyledAccordionSummary = styled(AccordionSummary)(({ theme }) => ({
-	color: "#90A4AE",
-	[theme.breakpoints.down("sm")]: {
-		padding: "0px 10%",
-	},
-	"& .Mui-expanded": {
-		color: "#3D3DD9",
-		boxShadow: "none",
-		">p": {
-			fontFamily: "IvyPresto Display",
-			fontSize: 24,
-			transition: "all 300ms ease-in-out",
-		},
-	},
+  color: "#90A4AE",
+  [theme.breakpoints.down("sm")]: {
+    padding: "0px 8%",
+  },
+  "& .Mui-expanded": {
+    color: "#3D3DD9",
+    boxShadow: "none",
+    ">p": {
+      fontFamily: "IvyPresto Display",
+      fontSize: 24,
+      transition: "all 300ms ease-in-out",
+    },
+  },
 }));
 
 export const AccordionTypography = styled(Typography)({
-	fontFamily: "IvyPresto Display",
-	fontSize: 20,
+  fontFamily: "IvyPresto Display",
+  fontSize: 20,
 });
 
-export const StyledCard = styled(Card)({
-	background: "transparent",
-});
+export const StyledCard = styled(Card)(({ theme }) => ({
+  background: "transparent",
+  [theme.breakpoints.up("md")]: {
+    width: "40%",
+  },
+}));
 
 export const StyledCardContent = styled(CardContent)(({ theme }) => ({
-	background: "#2E2E80",
-	color: "#FFF",
-	height: "18vh",
-	overflow: "hidden",
-	[theme.breakpoints.only("sm")]: {
-		padding: 14,
-		height: "26vh",
-	},
-	[theme.breakpoints.only("md")]: {
-		height: "25vh",
-	},
-	[theme.breakpoints.only("lg")]: {
-		height: "20vh",
-	},
-	">div": {
-		fontFamily: "IvyPresto Display",
-		fontSize: 24,
-		[theme.breakpoints.down("sm")]: {
-			fontSize: 18,
-		},
-		marginBottom: 0,
-	},
-	">p": {
-		fontFamily: "Poppins",
-		fontSize: 15,
-		[theme.breakpoints.down("sm")]: {
-			fontSize: 14,
-		},
-		width: "80%",
-	},
+  background: "#2E2E80",
+  color: "#FFF",
+  height: "28vh",
+  overflow: "hidden",
+  [theme.breakpoints.only("sm")]: {
+    padding: 18,
+    height: "26vh",
+  },
+  [theme.breakpoints.only("md")]: {
+    height: "25vh",
+  },
+  [theme.breakpoints.only("lg")]: {
+    height: "20vh",
+  },
+  ">div": {
+    fontFamily: "IvyPresto Display",
+    fontSize: 24,
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 18,
+    },
+    marginBottom: 0,
+  },
+  ">p": {
+    fontFamily: "Poppins",
+    fontSize: 15,
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 14,
+    },
+    width: "80%",
+  },
 }));
 
 export const StyledMask = styled("img")(({ theme }) => ({
-	position: "relative",
-	[theme.breakpoints.only("xl")]: {
-		bottom: "23vh",
-		left: "71%",
-	},
-	[theme.breakpoints.only("lg")]: {
-		bottom: "20vh",
-		left: "65%",
-	},
-	[theme.breakpoints.down("lg")]: {
-		bottom: "12vh",
-		left: "35%",
-	},
-	[theme.breakpoints.only("md")]: {
-		bottom: "27vh",
-		left: "50%",
-	},
-	[theme.breakpoints.only("sm")]: {
-		bottom: "30vh",
-		left: "42%",
-	},
-	[theme.breakpoints.down("sm")]: {
-		bottom: "22vh",
-		left: "12%",
-	},
-	[theme.breakpoints.only("xs")]: {
-		bottom: "14vh",
-		left: "12%",
-	},
+  position: "relative",
+  [theme.breakpoints.only("xl")]: {
+    bottom: "23vh",
+    left: "71%",
+  },
+  [theme.breakpoints.only("lg")]: {
+    bottom: "20vh",
+    left: "65%",
+  },
+  [theme.breakpoints.down("lg")]: {
+    bottom: "12vh",
+    left: "35%",
+  },
+  [theme.breakpoints.only("md")]: {
+    bottom: "27vh",
+    left: "50%",
+  },
+  [theme.breakpoints.only("sm")]: {
+    bottom: "30vh",
+    left: "42%",
+  },
+  [theme.breakpoints.down("sm")]: {
+    bottom: "22vh",
+    left: "12%",
+  },
+  [theme.breakpoints.only("xs")]: {
+    bottom: "14vh",
+    left: "12%",
+  },
 }));
 
 export const NextButton = styled("div")(({ theme }) => ({
-	width: 50,
-	height: 50,
-	borderRadius: 50 / 2,
-	position: "relative",
-	zIndex: 2,
-	background: "#3D3DD9",
-	left: "95%",
-	[theme.breakpoints.only("xs")]: {
-		bottom: "34vh",
-		left: " 84%",
-		width: "40px",
-		height: "40px",
-		borderRadius: "20px",
-	},
-	[theme.breakpoints.only("sm")]: {
-		bottom: "47vh",
-		left: "88%",
-	},
-	[theme.breakpoints.only("md")]: {
-		bottom: "44vh",
-		left: "88%",
-	},
-	[theme.breakpoints.only("xl")]: {
-		bottom: "36vh",
-	},
-	[theme.breakpoints.only("lg")]: {
-		bottom: "29vh",
-	},
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-	cursor: "pointer",
+  width: 50,
+  height: 50,
+  borderRadius: 50 / 2,
+  position: "relative",
+  zIndex: 2,
+  background: "#3D3DD9",
+  left: "95%",
+  [theme.breakpoints.only("xs")]: {
+    bottom: "24vh",
+    left: " 92%",
+    width: "40px",
+    height: "40px",
+    borderRadius: "20px",
+  },
+  [theme.breakpoints.only("sm")]: {
+    bottom: "47vh",
+    left: "92%",
+  },
+  [theme.breakpoints.only("md")]: {
+    bottom: "44vh",
+    left: "92%",
+  },
+  [theme.breakpoints.only("xl")]: {
+    bottom: "24vh",
+  },
+  [theme.breakpoints.only("lg")]: {
+    bottom: "25vh",
+  },
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
 }));
 
 export const NextArrowDiv = styled("div")(({ theme }) => ({
-	background: "transparent",
-	height: 40,
-	width: 40,
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-	"> svg": {
-		fill: "#3D3DD9",
-	},
-	position: "relative",
-	top: 0,
-	left: "96%",
-	cursor: "pointer",
-	[theme.breakpoints.down("sm")]: {
-		top: "-0.3vh",
-		left: "90%",
-	},
+  background: "transparent",
+  height: 40,
+  width: 40,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  "> svg": {
+    fill: "#3D3DD9",
+  },
+  position: "relative",
+  top: 0,
+  left: "96%",
+  cursor: "pointer",
+  [theme.breakpoints.down("sm")]: {
+    top: "-0.3vh",
+    left: "90%",
+  },
 }));
 export const PrevArrowDiv = styled("div")(({ theme }) => ({
-	background: "transparent",
-	height: 40,
-	width: 40,
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-	"> svg": {
-		fill: "#555",
-	},
-	position: "relative",
-	top: "51vh",
-	left: "92%",
-	cursor: "pointer",
-	[theme.breakpoints.up("sm")]: {
-		top: 646,
-	},
-	[theme.breakpoints.down("sm")]: {
-		top: "96vh",
-		left: "80%",
-	},
+  background: "transparent",
+  height: 40,
+  width: 40,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  "> svg": {
+    fill: "#555",
+  },
+  position: "relative",
+  top: "51vh",
+  left: "92%",
+  cursor: "pointer",
+  [theme.breakpoints.up("sm")]: {
+    top: 646,
+  },
+  [theme.breakpoints.down("sm")]: {
+    top: "96vh",
+    left: "80%",
+  },
 }));
 export const StyledAccordionDetails = styled(AccordionDetails)({
-	background: "transparent",
+  background: "transparent",
 });
 export const StyledAccordionDetails2 = styled(AccordionDetails)({
-	marginTop: -40,
-	background: "transparent",
+  marginTop: -40,
+  background: "transparent",
 });
 
 export const FloorPlans = styled("div")({
-	width: "100%",
-	height: "100%",
-	position: "absolute",
-	top: 0,
-	background: "#3d3dd978",
-	display: "flex",
-	alignItems: "center",
-	padding: 20,
-	borderRadius: "inherit",
-	justifyContent: "space-between",
+  width: "100%",
+  height: "100%",
+  position: "absolute",
+  top: 0,
+  background: "#3d3dd978",
+  display: "flex",
+  alignItems: "center",
+  padding: 20,
+  borderRadius: "inherit",
+  justifyContent: "space-between",
 });
 
 export const Text1 = styled(Typography)(({ theme }) => ({
-	fontFamily: "IvyPresto Display",
-	fontSize: 60,
-	[theme.breakpoints.down("md")]: {
-		fontSize: 30,
-	},
+  fontFamily: "IvyPresto Display",
+  fontSize: 24,
+  color: "#fff",
+  [theme.breakpoints.down("md")]: {
+    fontSize: 30,
+  },
 }));
 export const Text2 = styled(Typography)(({ theme }) => ({
-	fontFamily: "Poppins",
-	fontWeight: 500,
-	fontSize: 22,
-	[theme.breakpoints.down("md")]: {
-		fontSize: 15,
-	},
+  fontFamily: "Poppins",
+  fontWeight: 500,
+  fontSize: 18,
+  color: "#fff",
+  [theme.breakpoints.down("md")]: {
+    fontSize: 15,
+  },
 }));
 export const TextBox = styled("div")({
-	display: "flex",
-	flexDirection: "column",
+  display: "flex",
+  flexDirection: "column",
 });
 
-export const PDFDownload = styled(FileDownloadIcon)({
-	height: "80%",
-	width: "20%",
+export const PDFDownload = styled("img")({
+  height: "65%",
+  width: "30%",
+});
+
+export const ModelImage = styled("img")(({ theme }) => ({
+  maxWidth: "fit-content",
+  width: "300px",
+  height: "300px",
+  [theme.breakpoints.down("sm")]: {
+    maxWidth: "fit-content",
+    width: "115px",
+    height: "115px",
+    marginTop: "8px",
+  },
+}));
+
+export const ImageContainer = styled("div")({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  flexWrap: "wrap",
 });
