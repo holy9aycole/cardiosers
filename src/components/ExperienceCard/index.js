ff5f2753af3c383192e10a6caf8997a28addbae4
Ashish Yadav <yadavashishkumar97@gmail.com>
2022-03-17 13:31:56 +0530

    splash-screen

--- src/components/ExperienceCard/index.js
+++ src/components/ExperienceCard/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Typography, Grid, Card } from "@mui/material";
-// import moment from "moment";
 import tag from "assets/images/tag-icon.png";
-// import clock from "assets/images/clock-icon.png";
 import mask2 from "assets/images/mask2.png";
 
 import {
   CardFooter,
   useStyles,
-  // Time,
   FooterShadow,
   CardHeader,
   ImageTag,
@@ -32,12 +29,6 @@ export default function ExperienceCard(props) {
       <CardFooter>
         <Typography className="text2">{props.name}</Typography>
         <Typography className="text3">{props.description}</Typography>
-        {/* <Time>
-          <img className="clock" src={clock} alt="clock" />
-          <Typography className="ago">
-            {moment(props.time).fromNow()}
-          </Typography>
-        </Time> */}
         <FooterShadow src={mask2} alt="footer shadow" />
       </CardFooter>
     </Grid>
