00dccf6d6c6295160eafa4f145a87c82245da344
bernandez <alouzehm@gmail.com>
2022-11-21 11:25:02 -0600

    Added text on the home page

--- src/component/ui/home/Poster.jsx
+++ src/component/ui/home/Poster.jsx
@@ -5,10 +5,9 @@ const Poster = () => (
     <div className="poster__info">
       <h1 className="poster___title">For Private Clinics and medical center</h1>
       <span className="poster__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
-        consequuntur! Aperiam sequi quas dolore labore dicta similique animi.
-        Provident repudiandae eius facere odio excepturi illum officia et
-        pariatur quo. Reiciendis.
+      Cardiosers is webapplication which is used as a  medical tool  by health facilities for the diagnoses  
+      of cardiomegally with the use of digital chest x ray.This platform will ease the analyses and detection of Cardiiomegally 
+      from digital chest x-ray.
       </span>
       <button className="poster__btn">Get Started</button>
     </div>
